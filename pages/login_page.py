e85dd47575ab6854c3a093b0fb52ecb0715f7147
Maria <mariapershikova@yandex.ru>
2019-12-11 14:56:11 +0300

    added LoginPage implementation

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from pages.locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):  # IS A
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, "Url doesn't contain substring 'login'"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
